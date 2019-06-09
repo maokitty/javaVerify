2848687dc3a6e558b15c3244ba5480134ae050d4
maokitty <toolpackage@163.com>
2019-06-09 23:50:30 +0800

    添加rxjava2

--- rxjava2/src/main/java/paxi/maokitty/verify/ObservableVerify.java
+++ rxjava2/src/main/java/paxi/maokitty/verify/ObservableVerify.java
@@ -1,6 +1,7 @@
 package paxi.maokitty.verify;
 
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +9,17 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by maokitty on 19/6/9.
  */
-public class SimpleUse {
-    private static Logger LOG = LoggerFactory.getLogger(SimpleUse.class);
+public class ObservableVerify {
+    private static Logger LOG = LoggerFactory.getLogger(ObservableVerify.class);
     public static void main(String[] args) {
-        Observable.just(1).subscribe(new Consumer<Integer>() {
+        justVerify();
+    }
+
+    private static void justVerify() {
+        Disposable disposable = Observable.just(1).subscribe(new Consumer<Integer>() {
             @Override
             public void accept(Integer integer) throws Exception {
-               LOG.info("accept:{}",integer);
+                LOG.info("accept:{}", integer);
             }
         });
     }
